ee6df7711794be98466fccddc04210c5526a6820
Gompei <shoueigonpei6028@gmail.com>
2021-11-29 20:56:44 +0900

    shell修正・追加

--- shell/put_test_data.sh
+++ shell/put_test_data.sh
@@ -1,23 +1,16 @@
 #!/usr/bin/env bash
 
-: create dynamodb table
-aws dynamodb create-table \
-  --table-name "$DYNAMODB_TABLE_NAME" \
-  --attribute-definitions AttributeName=id,AttributeType=N \
-  --key-schema AttributeName=id,KeyType=HASH \
-  --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
-  --endpoint-url "$DYNAMODB_ENDPOINT" > /dev/null 2>&1
+DYNAMODB_TABLE_NAME="article_table"
 
 : put test data
-for id in $(seq 100)
+for id in $(seq 10)
 do
   # Mac
-  # DATE=$(date -v-"$id"d '+%Y-%m-%dT%H:%M:%S%z')
+  DATE=$(date -v-"$id"d '+%Y-%m-%dT%H:%M:%S%z')
   # Linux
-  DATE=$(date --date "$id days ago" '+%Y-%m-%dT%H:%M:%S%z')
+  # DATE=$(date --date "$id days ago" '+%Y-%m-%dT%H:%M:%S%z')
   aws dynamodb put-item  \
     --region us-east-1 \
     --table-name "$DYNAMODB_TABLE_NAME" \
-    --endpoint-url "$DYNAMODB_ENDPOINT" \
     --item "{ \"id\": { \"N\": \"$id\"},\"create_time_stamp\": {\"S\": \"$DATE\"},\"update_time_stamp\": {\"S\": \"$DATE\"},\"title\": {\"S\": \"example\"},\"sub_title\": { \"S\": \"example\"},\"image_url\": {\"S\": \"example\"},\"category_tag\": {\"S\": \"example\"},\"description\": {\"S\": \"example\"},\"content\": {\"S\": \"example\"}}"
 done
